a295a0efa8de55d38e8c9f1afd7e722aa7daa9df
Soumik Pal <pal.soumik33@gmail.com>
2022-05-12 14:55:16 +0530

    Adding Logo and Header Template

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../../resources/wellsfargo.png'
+
+
+function Logo() {
+    return (
+        <div style={{ display: "flex", width: 150,height:150 }}>
+            
+            <img
+                src={logo}
+            />
+            
+        </div>
+    );
+}
+
+export default Logo
+
+
+
---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>Name Pronouncation Tool</h1><br/>
+            <h4>Your guide to effective Name Pronouncation</h4>
+        </div>
+    )
+}
+
+export default Title
