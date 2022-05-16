c41ba912fde588cf251935d06188fbb165f63eaa
Soumik Pal <pal.soumik33@gmail.com>
2022-05-16 11:05:35 +0530

    Adding Search

--- src/components/PronunciationsList.js
+++ src/components/PronunciationsList.js
@@ -15,6 +15,7 @@ const PronunciationsList = () => {
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [searchPronunciation, setSearchPronunciation] = useState("");
   //const [currentPronunciationAudio,setCurrentPronunciationAudio]=useState("");
+  const [searchShow, setSearchShow] = useState(false); 
 
   const pronunciations = useSelector(state => state.pronunciations);
   const defaultpronunciations=useSelector(state=>state.defaultpronunciations);
@@ -23,6 +24,100 @@ const PronunciationsList = () => {
   useEffect(() => {
     dispatch(retrievePronunciation());
   }, []);
+  const filteredResult = pronunciations.filter(
+    person => {
+      return (
+        person
+          .name
+          .toLowerCase()
+          .includes(searchPronunciation.toLowerCase())
+      );
+    }
+  );
+
+  const handleChange = e => {
+    setSearchPronunciation(e.target.value);
+    if (e.target.value === "") {
+      setSearchShow(false);
+    }
+    else {
+      setSearchShow(true);
+    }
+  };
+
+  function searchList() {
+    if (searchShow) {
+      return (
+        <div className="list row">
+        <div className="col-md-6">
+        <h4>Employee List</h4>
+
+        <ul className="list-group">
+          {filteredResult &&
+            filteredResult.map((pronunciation, index) => (
+              <li
+                className={
+                  "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActivePronunciation(pronunciation, index)}
+                key={index}
+              >
+                {pronunciation.name}
+              </li>
+            ))}
+        </ul>
+
+        {/* <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllPronunciations}
+        >
+          Remove All
+        </button> */}
+      </div>
+      <div className="col-md-6">
+        {currentPronunciation ? (
+          <div>
+            <h4>Pronunciation</h4>
+            <div>
+              <label>
+                <strong>Name:</strong>
+              </label>{" "}
+              {currentPronunciation.name}
+            </div>
+            <div>
+              <label>
+                <strong>Pronunciation:</strong>
+              </label>{" "}
+              {currentPronunciation.defaultPronunciation}
+            </div>
+            <div>
+              <button onClick={() => playAudio(currentPronunciation.defaultAudio)}>
+                <span>Play Audio</span>
+              </button>
+            </div>
+
+
+            {/* <Link
+              to={"/pronunciations/" + currentPronunciation.id}
+              className="badge badge-warning"
+            >
+              Edit
+            </Link> */}
+          </div>
+        ) : (
+          <div>{filteredResult.length>0?
+            (<p>Please click on a Pronunciation</p>):
+                  (<p>No Employee Found</p>)}
+          </div>
+        )}
+      </div>
+      </div>
+        
+        
+        
+      );
+    }
+  }
 
   var context = new AudioContext();
   var source = null;
@@ -40,6 +135,14 @@ const PronunciationsList = () => {
   const onChangeSearchPronunciation = e => {
     const searchPronunciation = e.target.value;
     setSearchPronunciation(searchPronunciation);
+    setCurrentPronunciation(null);
+    setCurrentIndex(-1);
+    if (e.target.value === "") {
+      setSearchShow(false);
+    }
+    else {
+      setSearchShow(true);
+    }
   };
 
   const refreshData = () => {
@@ -111,7 +214,7 @@ const PronunciationsList = () => {
           <input
             type="text"
             className="form-control"
-            placeholder="Search Name Pronunciation"
+            placeholder="Search Name / Get Default"
             value={searchPronunciation}
             onChange={onChangeSearchPronunciation}
           />
@@ -133,68 +236,7 @@ const PronunciationsList = () => {
         </button>
 
       </div>
-      <div className="col-md-6">
-        <h4>Employee List</h4>
-
-        <ul className="list-group">
-          {pronunciations &&
-            pronunciations.map((pronunciation, index) => (
-              <li
-                className={
-                  "list-group-item " + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActivePronunciation(pronunciation, index)}
-                key={index}
-              >
-                {pronunciation.name}
-              </li>
-            ))}
-        </ul>
-
-        {/* <button
-          className="m-3 btn btn-sm btn-danger"
-          onClick={removeAllPronunciations}
-        >
-          Remove All
-        </button> */}
-      </div>
-      <div className="col-md-6">
-        {currentPronunciation ? (
-          <div>
-            <h4>Pronunciation</h4>
-            <div>
-              <label>
-                <strong>Name:</strong>
-              </label>{" "}
-              {currentPronunciation.name}
-            </div>
-            <div>
-              <label>
-                <strong>Pronunciation:</strong>
-              </label>{" "}
-              {currentPronunciation.defaultPronunciation}
-            </div>
-            <div>
-              <button onClick={() => playAudio(currentPronunciation.defaultAudio)}>
-                <span>Play Audio</span>
-              </button>
-            </div>
-
-
-            {/* <Link
-              to={"/pronunciations/" + currentPronunciation.id}
-              className="badge badge-warning"
-            >
-              Edit
-            </Link> */}
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a Pronunciation...</p>
-          </div>
-        )}
-      </div>
+      {searchList()}
     </div>
   );
 };
