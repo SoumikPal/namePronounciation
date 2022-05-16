08ca8efddd3832fb9ebebeeb9ea10110c227778d
Satish Yele <yelesatish@gmail.com>
2022-05-16 18:43:36 +0530

    get token url change

--- src/components/util/token_util.js
+++ src/components/util/token_util.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export async function getTokenOrRefresh() {
   try {
-    const res = await axios.get('/api/get-speech-token');
-    const token = res.data.token;
-    const region = res.data.region;
+    const res = await axios.get('https://namepronunciationserviceptmvn.azurewebsites.net/api/token');
+    const token = res.data;
+    const region = 'eastus';
 
     console.log('Token fetched from back-end: ' + token);
     return { authToken: token, region: region };
